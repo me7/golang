2c783bfc80bd3e51e96c79e4762f90413b581032
me7 <gampolt@gmail.com>
2016-12-15 10:58:12 +0700

    simplify build.cmd

--- mongo/build.cmd
+++ mongo/build.cmd
@@ -1,11 +1,4 @@
-setlocal
-%~d0
-cd %~p0
-for %%* in (.) do set CURR=%%~nx*
-echo Building %CURR%
-set FLAGS=-ldflags "-s -w"
 set GOOS=windows
 set GOARCH=386
-go build %FLAGS% -o %CURR%.exe
-upx --best %CURR%.exe
-endlocal
+go build -ldflags "-s -w"
+upx --best *.exe
